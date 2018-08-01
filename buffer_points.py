6c1c116485ac63f5fe456a4674853ac9c030adb0
Liam <41880174+Liam96@users.noreply.github.com>
2018-08-01 08:15:00 +0800

    Create buffer_points.py

---
+++ buffer_points.py
@@ -0,0 +1,12 @@
+# Script to buffer the points so they have a 90 metre diameter
+import arcpy
+# ===================
+# To be used in the Python Window in ArcMap
+outFolder = "E:\wgp_data\Year_by_Year\Buffered_Sampled_Points\\" # Out Path
+bufferDist = 45   # Uses map units
+# ===================
+
+layers = arcpy.mapping.ListLayers(arcpy.mapping.MapDocument("CURRENT")) # List all the layers in the mxd
+for layer in layers: # Loop through the layers
+    outPath = outFolder + str(layer.name) + "_buffered" # Create the file name
+    arcpy.Buffer_analysis(layer, outPath, bufferDist) # Perform the buffer
